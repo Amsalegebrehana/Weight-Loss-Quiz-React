56dafb801a7cfab6c42a1faaa112014663a0d033
Amsalegebrehana <se.amsale.gebrehana@gmail.com>
2024-01-31 21:23:06 +0300

    question flow

---
+++ src/pages/Result.js
@@ -0,0 +1,14 @@
+
+
+const Result = ({response}) => {
+
+    console.log("results response",response);
+
+    return (
+        <div>
+            <h1>Result</h1>
+        </div>
+    )
+}
+
+export default Result;
